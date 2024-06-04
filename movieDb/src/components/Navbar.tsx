faea15fb2e1547ec23f473ba41470260ca89807b
cezarm84 <cezarmh@gmail.com>
2024-06-04 23:19:37 +0200

    creat a initial navbar component with css

---
+++ movieDb/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { useStore } from '../store/store';
+import { useNavigate } from 'react-router-dom';
+import './styles/navbar.css';
+
+const Navbar: React.FC = () => {
+  const logout = useStore((state) => state.logout);
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar">
+      <button onClick={() => navigate('/home')}>Home</button>
+      <button onClick={logout}>Logout</button>
+    </nav>
+  );
+};
+
+export default Navbar;
