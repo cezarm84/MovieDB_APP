9f91af2140d4dff060381605cda6c2d463a98326
cezarm84 <cezarmh@gmail.com>
2024-06-06 00:10:29 +0200

    update app configurations

--- movieDb/src/App.tsx
+++ movieDb/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignupPage';
-import MovieDetailsPage from './pages/MoviePage';
+import MovieDetailsPage from './pages/MovieDetailsPage';
+import FavoritesPage from './pages/FavoritesPage';
 import { useStore } from './store/store';
 
 const App: React.FC = () => {
@@ -15,6 +16,7 @@ const App: React.FC = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/movie/:id" element={<MovieDetailsPage />} />
+        <Route path="/favorites" element={user ? <FavoritesPage /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   );
