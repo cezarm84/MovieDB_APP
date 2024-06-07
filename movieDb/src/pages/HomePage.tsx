0463a228027803a106b1f771dfebc680857527d0
cezarm84 <cezarmh@gmail.com>
2024-06-07 22:08:57 +0200

    edit home page

--- movieDb/src/pages/HomePage.tsx
+++ movieDb/src/pages/HomePage.tsx
@@ -3,18 +3,35 @@ import MovieList from '../components/MovieList';
 import AddMovieForm from '../components/AddMovieForm';
 import './styles/homePage.css';
 import { useStore } from '../store/store';
+import React, { useEffect } from 'react';
+
 
 const HomePage: React.FC = () => {
   const movies = useStore((state) => state.movies);
- 
+  const getMovies = useStore((state) => state.getMovies);
+  const getApiKey = useStore((state) => state.getApiKey);
+
+  useEffect(() => {
+    const initialize = async () => {
+      await getApiKey();
+      getMovies();
+    };
+    initialize();
+  }, [getMovies, getApiKey]);
 
   return (
     <div className="home-page">
       <Navbar />
-      <AddMovieForm />
-      <MovieList movies={movies} />
+      <div className="add-movie-form">
+        <AddMovieForm />
+      </div>
+      <div className="movie-list">
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 };
+
+
 
 export default HomePage;
