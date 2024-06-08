b68efed4b200bda110bddfb619bd0bb5a2f3cb8a
cezarm84 <cezarmh@gmail.com>
2024-06-08 14:10:50 +0200

    tweking the style

--- movieDb/src/components/MovieList.tsx
+++ movieDb/src/components/MovieList.tsx
@@ -8,16 +8,9 @@ interface MovieListProps {
 }
 
 const MovieList: React.FC<MovieListProps> = ({ movies }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex] = useState(0);
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? movies.length - 1 : prevIndex - 1));
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === movies.length - 1 ? 0 : prevIndex + 1));
-  };
-
+ 
   return (
     <div className="movie-list-container">
       <div className="movie-list">
