fe273e1af8448031be167c798b0e825f1faf5594
cezarm84 <cezarmh@gmail.com>
2024-06-08 02:07:12 +0200

    edit FavoPage

--- movieDb/src/pages/FavoritesPage.tsx
+++ movieDb/src/pages/FavoritesPage.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import './styles/favoritesPage.css';
 
 const FavoritesPage = () => {
-  const { movies, getMovies, toggleFavorite } = useStore();
+  const { movies, getMovies, } = useStore();
 
   useEffect(() => {
     getMovies();
@@ -12,31 +12,8 @@ const FavoritesPage = () => {
 
   const favoriteMovies = movies.filter(movie => movie.isFavorite);
 
-  const handleToggleFavorite = (id: string) => {
-    toggleFavorite(id);
-  };
-
   return (
-<<<<<<< HEAD
-    <div className="fav-page">
-      <Navbar />
-      <div className="content">
-        <h1 className="title">Favorite Movies</h1>
-        <ul className="movie-list">
-          {favoriteMovies.map((movie) => (
-            <li key={movie.id} className="movie-item">
-              <img src={movie.posterUrl} alt={movie.title} className="movie-poster" />
-              <div className="movie-details">
-                <h2>{movie.title}</h2>
-                <button onClick={() => handleToggleFavorite(movie.id)}>
-                  {movie.isFavorite ? "Remove from Favorites" : "Add to Favorites"}
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-=======
+
       <div className="fav-page">
         <Navbar/>
       <ul>
@@ -44,7 +21,7 @@ const FavoritesPage = () => {
           <li key={movie.id}>{movie.title}</li>
         ))}
       </ul>
->>>>>>> 2ea5445d0e0e6ff66a9f702f698ac68e87a5d631
+
     </div>
   );
 };
