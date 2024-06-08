5d7c1879530382084b8e05d16daeed14cb61aa31
cezarm84 <cezarmh@gmail.com>
2024-06-08 02:12:03 +0200

    hotfix favopage

--- movieDb/src/pages/FavoritesPage.tsx
+++ movieDb/src/pages/FavoritesPage.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import './styles/favoritesPage.css';
 
 const FavoritesPage = () => {
-  const { movies, getMovies, } = useStore();
+  const { movies, getMovies, toggleFavorite } = useStore();
 
   useEffect(() => {
     getMovies();
@@ -13,15 +13,24 @@ const FavoritesPage = () => {
   const favoriteMovies = movies.filter(movie => movie.isFavorite);
 
   return (
-
-      <div className="fav-page">
-        <Navbar/>
-      <ul>
-        {favoriteMovies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
-
+    <div className="fav-page">
+      <Navbar />
+      <div className="content">
+        <h1 className="title">Favorite Movies</h1>
+        <ul className="movie-list">
+          {favoriteMovies.map((movie) => (
+            <li key={movie.id} className="movie-item">
+              <img src={movie.poster} alt={movie.title} className="movie-poster" />
+              <div className="movie-details">
+                <h2>{movie.title}</h2>
+                <button onClick={() => toggleFavorite(movie.id)}>
+                  {movie.isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
