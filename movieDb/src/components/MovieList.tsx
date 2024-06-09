c6d8b11e83cea049d2830fc873e54200759768b8
cezarm84 <cezarmh@gmail.com>
2024-06-09 21:13:59 +0200

    tweaking the css at movie list

--- movieDb/src/components/MovieList.tsx
+++ movieDb/src/components/MovieList.tsx
@@ -1,22 +1,39 @@
+import React, { useState } from 'react';
 import { Movie } from '../model/Movie';
-import MovieCard from './MovieCard';
-import './styles/movieList.css'; 
+import  MovieCard  from './MovieCard';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import './styles/movieList.css';
 
 interface MovieListProps {
   movies: Movie[];
 }
 
 const MovieList: React.FC<MovieListProps> = ({ movies }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? movies.length - 1 : prevIndex - 1));
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === movies.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div className="movie-list-container">
       <div className="movie-list">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
+        {movies.map((movie, index) => (
+          <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+            <MovieCard movie={movie} />
+          </div>
         ))}
+      </div>
+      <div className="slider-controls">
+        <div className="arrow" onClick={goToPrevious}><FaArrowLeft /></div>
+        <div className="arrow" onClick={goToNext}><FaArrowRight /></div>
       </div>
     </div>
   );
 };
 
 export default MovieList;
-
